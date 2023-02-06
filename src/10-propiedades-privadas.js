b51390f29a59b2b3749ce2a7e2b2db4c874375e6
Christian <c.espinoza.code@gmail.com>
2023-02-06 09:26:42 -0500

    chris Object declarated

--- src/10-propiedades-privadas.js
+++ src/10-propiedades-privadas.js
@@ -1,3 +1,12 @@
 function requiredParam(param) {
   throw new Error(`${param} is required`);
 }
+
+
+// Declaración del objeto
+const chris = createStudent({
+  name: 'Christian',
+  age: 24,
+  email: 'c.espinoza.code@gmail.com',
+  twitter: 'chriscodex',
+});
