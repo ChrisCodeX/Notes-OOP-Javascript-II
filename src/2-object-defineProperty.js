2490d02dd9be9383b749614d51406405da40b90f
Christian <c.espinoza.code@gmail.com>
2023-02-05 19:01:55 -0500

    object define property example added

--- src/2-object-defineProperty.js
+++ src/2-object-defineProperty.js
@@ -1,3 +1,4 @@
+/* Object */
 const chris = {
   name: 'Christian',
   age: 24,
@@ -10,9 +11,10 @@ const chris = {
   }
 }
 
-// Adding additional propperties
+/* Adding additional propperties */
 Object.defineProperty(chris, 'navigator', {
   value: 'Chrome',
+  // Esta propiedad no se muestra como enumerada (Object.keys)
   enumerable: false,
   writable: true,
   configurable: true
@@ -21,6 +23,7 @@ Object.defineProperty(chris, 'navigator', {
 Object.defineProperty(chris, 'editor', {
   value: 'VSCode',
   enumerable: true,
+  // Esta propiedad no puede cambiarse, pero sí eliminarse
   writable: false,
   configurable: true
 })
@@ -29,8 +32,32 @@ Object.defineProperty(chris, 'terminal', {
   value: 'WSL',
   enumerable: true,
   writable: true,
+  // La propiedad no puede editarse, pero sí eliminarse
   configurable: false
 })
 
-console.log(chris);
+Object.defineProperty(chris, 'PruebaNasa', {
+  value: 'extraterrestres',
+  enumerable: false,
+  writable: false,
+  configurable: false
+})
+
+/* Navegador Web */
+console.log('Object', chris);
 // Show keys - Navigator no aparece
+console.log('Object.keys:', Object.keys(chris));
+
+// Navigator aparece
+console.log('Object.getOwnPropertyNames:', Object.getOwnPropertyNames(chris));
+
+// Eliminar propiedades
+delete chris.navigator
+console.log('Chris Object', chris);
+
+/* Object Manipulation - Todas las propiedades */
+// Configurable: false
+Object.seal(chris);
+
+// Writtable: false
+Object.freeze(chris);
