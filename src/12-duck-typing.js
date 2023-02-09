1572affd1783b74b6eabcae227b84eb3078d2397
Christian <c.espinoza.code@gmail.com>
2023-02-08 23:16:27 -0500

    chris object declared

--- src/12-duck-typing.js
+++ src/12-duck-typing.js
@@ -1,3 +1,44 @@
 function requiredParam(param) {
   throw new Error(`${param} is required`);
 }
+
+function createLearningPath({
+  name = requiredParam('name'),
+  courses = []
+}) {
+  const private = {
+    _name: name,
+    _courses: courses,
+  };
+
+  const public = {
+    // Name property
+    get name() {
+      return private._name;
+    },
+    set name(newName) {
+      if (newName.length === 0) {
+        console.warn('Tu nombre debe tener al menos 1 caracter');
+        return
+      }
+      private._name = newName;
+    },
+
+    // Courses property
+    get courses() {
+      return private._courses;
+    },
+  };
+
+  return public
+}
+// Declaración del objeto
+const chris = createStudent({
+  name: 'Christian',
+  age: 24,
+  email: 'c.espinoza.code@gmail.com',
+  twitter: 'chriscodex',
+});
+
+console.log('Ver los LP de chris', chris.learningPaths);
+
